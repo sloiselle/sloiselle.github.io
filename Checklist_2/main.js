80ab396c06a038da04c6c60bc2bcab53d558a33d
Scott Loiselle <sloiselle@draftkings.com>
2016-05-10 10:35:48 -0400

    Wrong file

--- Checklist_2/main.js
+++ Checklist_2/main.js
@@ -1,44 +1,98 @@
-/*
-
-Appending: Independent Practice
-
-- Open the index.html file in your browser and play around with it first before attempting to complete these instructions.
-- Append two anchor tags to each list item (the ones added by clicking "Create new thing"):
-  - An anchortag to complete the task.
-  - An anchortag to delete the task (list item).
-
-- The complete task event handler should toggle a 'completed' class on the list item.
-So <li class="completed">
-*/
-
-
 'use strict';
-
-var $ = require('jquery');
-
-$(function() {
-  var yourName = prompt('What is your name?');
-
-  $('#name').html(yourName);
-
-  var $thingList = $('#fav-list');
-
-  var $button = $('#new-thing-button');
-  // button.onclick = function(event) {
-  $button.on('click', function(event) {
-    event.preventDefault();
-    MyApp.addToList($thingList);
-  });
-});
-
 var MyApp = {};
 
-MyApp.addToList = function(list) {
-  var $newLi = $('<li>');
-  var $newItemText = $('#new-thing');
-  $newLi.html($newItemText.val());
-  $newItemText.val('');
-  if ($newLi.html() !== '') {
-    list.append($newLi);
-  }
+// Compile element using handlebars
+MyApp.compileItem = function(item) {
+  var source = $('#to-do-template').html();
+  var template = Handlebars.compile(source);
+  return template(item);
 }
+
+// Note: this starter data is usually loaded from somewhere
+MyApp.todos = [{toDo: "Clean fridge"}, {toDo: "Take out Puppy"}, {toDo: "Finish work"} ];
+
+//Add to List MyApp.addToList
+
+MyApp.addToList = function(list) {
+  //On click of '$button', add the given item to the 'todos' array
+    var $newThingText = $('#new-thing').val();
+    $('#new-thing').val("");
+    MyApp.todos.push({toDo: $newThingText}); //need to add to todos list for iteration later
+    if($newThingText != ""){
+      list.append(MyApp.compileItem({toDo: $newThingText}));
+    }
+  };
+
+// Initial population of the list from todos array
+MyApp.initializeList = function(list) {
+  $.each(MyApp.todos, function() {
+    list.append(MyApp.compileItem(this));
+  });
+}
+
+// Remove both the data from the model/array and from the DOM
+MyApp.removeFromList = function($list, $item) {
+  var itemIndex = $item.index();
+
+  // Remove item from main data array
+  if (itemIndex > -1)
+      MyApp.todos.splice(itemIndex, 1);
+
+  // Remove dom element of item
+  $item.remove();
+}
+
+$(function() {
+
+  var $thingList = $('#fav-list'),
+      $button = $('#new-thing-button');
+
+  $($button).on('click', function(e){
+    e.preventDefault();
+    MyApp.addToList($thingList);
+  });
+
+  MyApp.initializeList($thingList);
+
+
+
+  
+
+  /************************* Event Handlers *************************/
+  
+
+  // Hover events for each list item in the to-do list. On hovering an element, its siblings get an inactive class applied
+  $thingList.on('mouseenter mouseleave', 'li', function(e){
+    if(e.type == 'mouseenter'){
+      $(this).removeClass('inactive');
+      $(this).siblings().addClass('inactive');
+    } else if (e.type == 'mouseleave') {
+      $(this).siblings().removeClass('inactive');
+    }
+
+
+  });
+
+  //Click on Complete
+  $thingList.on('click', 'button.complete', function(e){
+    $(this).children().toggleClass('fa-hand-o-right');
+    $(this).parent().toggleClass('completed');
+    $(this).children().toggleClass('fa-check');
+  });
+
+  //Click on Delete
+  $thingList.on('click', 'button.delete', function(e){
+    var $item = $(this).parent();
+    MyApp.removeFromList($thingList, $item);
+  });  
+
+
+  /************************* End Event Handlers *************************/
+});
+
+
+
+
+
+
+
